365a3e1be975ad3693fc1bab0ead84382f7d3d43
seweis <seweis@gmx.net>
2023-03-23 17:19:32 +0100

    first draft

--- src/env-check.py
+++ src/env-check.py
@@ -3,3 +3,9 @@ import os
 
 print(sys.executable)
 print(os.popen('python --version').read())
+
+
+with open('my_new_file.txt', 'w') as file:
+	file.write('This is a new file!')
+
+input('Say something!')
